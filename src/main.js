497f5182684571cf5e837b6f0e8fb8bd5d1bbc7a
Christian Scheer <christianscheer84@gmail.com>
2025-05-25 14:38:31 +0200

    copty empty vide revealjs setup

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/dracula.css'
+import Reveal from 'reveal.js'
+import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealMath from 'reveal.js/plugin/math/math.esm.js';
+import markdownfile from './markdown.md'
+
+document.querySelector('.reveal').innerHTML = `
+      <div class="slides">
+        <section data-markdown="${markdownfile}">
+      </div>
+`
+Reveal.initialize({
+    hash: true,
+    width: 1920,
+    height: 1080,
+    margin: 0.04,
+    minScale: 0.2,
+    maxScale: 2.0,
+    slideNumber: true,
+    plugins: [RevealMarkdown, RevealMath.KaTeX]
+});
---
+++ src/markdown.md
@@ -0,0 +1,6 @@
+## vite revealjs.setup
+empty presentation
+
+---
+### Slide with Picture
+![vite logo](/vite.svg "vite logo title")
